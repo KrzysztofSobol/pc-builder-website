2eb7257e3966b19d1b33f1b9514ab39b807511c5
Kex <krzysieksobol9@gmail.com>
2025-05-08 17:58:41 +0200

    tak, nie robimy

--- src/main/java/pcbuilder/website/repositories/UserRepository.java
+++ src/main/java/pcbuilder/website/repositories/UserRepository.java
@@ -6,7 +6,7 @@ import pcbuilder.website.models.entities.User;
 
 import java.util.UUID;
 
-@Repository
+@Repository // TAK, NIE ROBIMY !!!!!
 public interface UserRepository extends CrudRepository<User, UUID> {
 
 }
